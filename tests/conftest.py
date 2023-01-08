fa0a22c11c31b0e4adf135a9b3a9a29ab0ee9a2c
Jonathan Holford <>
2023-01-08 14:48:54 +0000

    We now have an option to specify use of non-Playwright chromium from the command line

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
-from typing import Dict, Generator
-
+import sys
+from typing import Dict
 import pytest
 
+from playwright.sync_api import Page
 from pages.home_page import IsomorphicLabsHomePage
 from pages.work_with_us_page import IsomorphicLabsWorkWithUsPage
-from playwright.sync_api import BrowserType, Browser, Page
 
 
 @pytest.fixture
@@ -17,8 +17,18 @@ def work_with_us_page(page: Page) -> IsomorphicLabsWorkWithUsPage:
     return IsomorphicLabsWorkWithUsPage(page)
 
 
-@pytest.fixture(scope="session")
-def browser_type_launch_args(browser_type_launch_args):
+def pytest_addoption(parser):
+    parser.addoption("--use-non-playwright-chromium",
+                     action="store", default="false", help="Playwright will try to use installed Chromium instead of the the Playwright-bundled version")
 
-    print(browser_type_launch_args)
-    return {**browser_type_launch_args, "executable_path": "/usr/bin/chromium-browser"}
+
+def __get_is_non_playwright_chromium_specified(request):
+    return request.config.getoption("--use-non-playwright-chromium").lower() == "true"
+
+
+@pytest.fixture(scope="session")
+def browser_type_launch_args(browser_type_launch_args: Dict, request) -> Dict:
+    if __get_is_non_playwright_chromium_specified(request):
+        return {**browser_type_launch_args, "executable_path": "/usr/bin/chromium-browser"}
+
+    return browser_type_launch_args
